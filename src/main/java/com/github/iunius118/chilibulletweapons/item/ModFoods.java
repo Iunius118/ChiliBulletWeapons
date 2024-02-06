4c48f1a2b7740ac3c56850f41d3e5ccd2be0a2b1
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-02-06 22:45:49 +0900

    Reduce nourishment of food items

--- src/main/java/com/github/iunius118/chilibulletweapons/item/ModFoods.java
+++ src/main/java/com/github/iunius118/chilibulletweapons/item/ModFoods.java
@@ -3,8 +3,8 @@ package com.github.iunius118.chilibulletweapons.item;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties CHILI_CHICKEN_SANDWICH = new FoodProperties.Builder().nutrition(6).saturationMod(1.2F).build();
-    public static final FoodProperties CHILI_FISH_SANDWICH = new FoodProperties.Builder().nutrition(6).saturationMod(1.2F).build();
-    public static final FoodProperties CHILI_MEAT_SANDWICH = new FoodProperties.Builder().nutrition(8).saturationMod(1.2F).build();
-    public static final FoodProperties CHILI_POTATO_SANDWICH = new FoodProperties.Builder().nutrition(6).saturationMod(1.0F).build();
+    public static final FoodProperties CHILI_CHICKEN_SANDWICH = new FoodProperties.Builder().nutrition(6).saturationMod(0.8F).build();
+    public static final FoodProperties CHILI_FISH_SANDWICH = new FoodProperties.Builder().nutrition(6).saturationMod(0.8F).build();
+    public static final FoodProperties CHILI_MEAT_SANDWICH = new FoodProperties.Builder().nutrition(8).saturationMod(0.8F).build();
+    public static final FoodProperties CHILI_POTATO_SANDWICH = new FoodProperties.Builder().nutrition(6).saturationMod(0.6F).build();
 }
