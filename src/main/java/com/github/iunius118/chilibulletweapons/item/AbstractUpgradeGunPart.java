82de45856f27efb50e9675a6e02fc26ed78d1d67
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-03-20 17:53:46 +0900

    Add item descriptions to tooltips of gun upgrade items

--- src/main/java/com/github/iunius118/chilibulletweapons/item/AbstractUpgradeGunPart.java
+++ src/main/java/com/github/iunius118/chilibulletweapons/item/AbstractUpgradeGunPart.java
@@ -1,14 +1,24 @@
 package com.github.iunius118.chilibulletweapons.item;
 
 import com.github.iunius118.chilibulletweapons.sounds.ModSoundEvents;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public abstract class AbstractUpgradeGunPart extends Item {
+    public static final String TOOLTIP_UPGRADE_GUN_1 = "tooltip.chilibulletweapons.upgrade_gun_1";
+    public static final String TOOLTIP_UPGRADE_GUN_2 = "tooltip.chilibulletweapons.upgrade_gun_2";
+    public static final String TOOLTIP_UPGRADE_GUN_3 = "tooltip.chilibulletweapons.upgrade_gun_3";
+
     public AbstractUpgradeGunPart(Properties properties) {
         super(properties);
     }
@@ -38,5 +48,13 @@ public abstract class AbstractUpgradeGunPart extends Item {
 
         player.playSound(ModSoundEvents.GUN_UPGRADE, 0.5F, 1.1F + level.getRandom().nextFloat() * 0.1F);
         return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(itemStack, level, tooltip, flag);
+        tooltip.add(Component.translatable(TOOLTIP_UPGRADE_GUN_1).withStyle(ChatFormatting.YELLOW));
+        tooltip.add(Component.translatable(TOOLTIP_UPGRADE_GUN_2).withStyle(ChatFormatting.YELLOW));
+        tooltip.add(Component.translatable(TOOLTIP_UPGRADE_GUN_3).withStyle(ChatFormatting.YELLOW));
     }
 }
