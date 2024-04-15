4c1798fbeee15c4506435a4580f094b0466a0597
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-04-15 20:48:41 +0900

    Add group values to recipes

--- src/main/java/com/github/iunius118/chilibulletweapons/data/ModRecipeProvider.java
+++ src/main/java/com/github/iunius118/chilibulletweapons/data/ModRecipeProvider.java
@@ -27,12 +27,14 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         /* Plants */
         // Seeds
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.CHILI_SEEDS)
+                .group(getItemId(ModItems.CHILI_SEEDS).toString())
                 .requires(ModItems.CURVED_CHILI)
                 .unlockedBy("has_curved_chili", has(ModItems.CURVED_CHILI))
                 .save(recipeOutput, getItemId(ModItems.CHILI_SEEDS));
 
         // Storage items
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.BULLET_CHILI_SACK)
+                .group(getItemId(ModItems.BULLET_CHILI_SACK).toString())
                 .pattern("bbb")
                 .pattern("bbb")
                 .pattern("bbb")
@@ -41,11 +43,13 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.BULLET_CHILI_SACK));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.BULLET_CHILI, 9)
+                .group(getItemId(ModItems.BULLET_CHILI).toString())
                 .requires(ModItems.BULLET_CHILI_SACK)
                 .unlockedBy("has_bullet_chili_sack", has(ModItems.BULLET_CHILI_SACK))
                 .save(recipeOutput, getItemId(ModItems.BULLET_CHILI) + "_from_sack");
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.CURVED_CHILI_SACK)
+                .group(getItemId(ModItems.CURVED_CHILI_SACK).toString())
                 .pattern("ccc")
                 .pattern("ccc")
                 .pattern("ccc")
@@ -54,6 +58,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.CURVED_CHILI_SACK));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.CURVED_CHILI, 9)
+                .group(getItemId(ModItems.CURVED_CHILI).toString())
                 .requires(ModItems.CURVED_CHILI_SACK)
                 .unlockedBy("has_curved_chili_sack", has(ModItems.CURVED_CHILI_SACK))
                 .save(recipeOutput, getItemId(ModItems.CURVED_CHILI) + "_from_sack");
@@ -61,6 +66,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         /* Foods */
         // Sandwiches
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.CHILI_CHICKEN_SANDWICH, 2)
+                .group(getItemId(ModItems.CHILI_CHICKEN_SANDWICH).toString())
                 .requires(ModItems.CURVED_CHILI)
                 .requires(Items.COOKED_CHICKEN)
                 .requires(Items.BREAD)
@@ -68,6 +74,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.CHILI_CHICKEN_SANDWICH));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.CHILI_FISH_SANDWICH, 2)
+                .group(getItemId(ModItems.CHILI_FISH_SANDWICH).toString())
                 .requires(ModItems.CURVED_CHILI)
                 .requires(ModItemTags.FOODS_COOKED_FISH)
                 .requires(Items.BREAD)
@@ -75,6 +82,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.CHILI_FISH_SANDWICH));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.CHILI_MEAT_SANDWICH, 2)
+                .group(getItemId(ModItems.CHILI_MEAT_SANDWICH).toString())
                 .requires(ModItems.CURVED_CHILI)
                 .requires(ModItemTags.FOODS_COOKED_MEAT)
                 .requires(Items.BREAD)
@@ -82,6 +90,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.CHILI_MEAT_SANDWICH));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.CHILI_POTATO_SANDWICH, 2)
+                .group(getItemId(ModItems.CHILI_POTATO_SANDWICH).toString())
                 .requires(ModItems.CURVED_CHILI)
                 .requires(Items.BAKED_POTATO)
                 .requires(Items.BREAD)
@@ -90,27 +99,32 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
         // Half-sized sandwiches
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.HALF_CHILI_CHICKEN_SANDWICH, 2)
+                .group(getItemId(ModItems.HALF_CHILI_CHICKEN_SANDWICH).toString())
                 .requires(ModItems.CHILI_CHICKEN_SANDWICH)
                 .unlockedBy("has_full_sandwich", has(ModItems.CHILI_CHICKEN_SANDWICH))
                 .save(recipeOutput, getItemId(ModItems.HALF_CHILI_CHICKEN_SANDWICH));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.HALF_CHILI_FISH_SANDWICH, 2)
+                .group(getItemId(ModItems.HALF_CHILI_FISH_SANDWICH).toString())
                 .requires(ModItems.CHILI_FISH_SANDWICH)
                 .unlockedBy("has_full_sandwich", has(ModItems.CHILI_FISH_SANDWICH))
                 .save(recipeOutput, getItemId(ModItems.HALF_CHILI_FISH_SANDWICH));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.HALF_CHILI_MEAT_SANDWICH, 2)
+                .group(getItemId(ModItems.HALF_CHILI_MEAT_SANDWICH).toString())
                 .requires(ModItems.CHILI_MEAT_SANDWICH)
                 .unlockedBy("has_full_sandwich", has(ModItems.CHILI_MEAT_SANDWICH))
                 .save(recipeOutput, getItemId(ModItems.HALF_CHILI_MEAT_SANDWICH));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.HALF_CHILI_POTATO_SANDWICH, 2)
+                .group(getItemId(ModItems.HALF_CHILI_POTATO_SANDWICH).toString())
                 .requires(ModItems.CHILI_POTATO_SANDWICH)
                 .unlockedBy("has_curved_chili", has(ModItems.CHILI_POTATO_SANDWICH))
                 .save(recipeOutput, getItemId(ModItems.HALF_CHILI_POTATO_SANDWICH));
 
         // Pasta
         ShapedRecipeBuilder.shaped(RecipeCategory.FOOD, ModItems.PASTA_OIL_AND_CHILI)
+                .group(getItemId(ModItems.PASTA_OIL_AND_CHILI).toString())
                 .pattern(" c ")
                 .pattern("www")
                 .pattern(" b ")
@@ -122,6 +136,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
         // Fried chili pepper
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.FRIED_CHILI_PEPPER, 3)
+                .group(getItemId(ModItems.FRIED_CHILI_PEPPER).toString())
                 .requires(ModItems.CURVED_CHILI, 3)
                 .requires(Items.WHEAT)
                 .unlockedBy("has_curved_chili", has(ModItems.CURVED_CHILI))
@@ -130,12 +145,14 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         /* Weapons */
         // Bullet
         ShapelessRecipeBuilder.shapeless(RecipeCategory.COMBAT, ModItems.CHILI_BULLET)
+                .group(getItemId(ModItems.CHILI_BULLET).toString())
                 .requires(ModItems.BULLET_CHILI)
                 .unlockedBy("has_bullet_chili", has(ModItems.BULLET_CHILI))
                 .save(recipeOutput, getItemId(ModItems.CHILI_BULLET));
 
         // Upgrade parts
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.UPGRADE_GUN_BAYONET)
+                .group(getItemId(ModItems.UPGRADE_GUN_BAYONET).toString())
                 .pattern("bbb")
                 .pattern("iis")
                 .pattern("bbb")
@@ -146,6 +163,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.UPGRADE_GUN_BAYONET));
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.UPGRADE_GUN_BARREL)
+                .group(getItemId(ModItems.UPGRADE_GUN_BARREL).toString())
                 .pattern("bbb")
                 .pattern("iii")
                 .pattern("bbb")
@@ -155,6 +173,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.UPGRADE_GUN_BARREL));
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.UPGRADE_GUN_MECHANISM)
+                .group(getItemId(ModItems.UPGRADE_GUN_MECHANISM).toString())
                 .pattern("bbb")
                 .pattern("pii")
                 .pattern("bbb")
@@ -166,6 +185,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
         // Guns
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ModItems.GUN)
+                .group(getItemId(ModItems.GUN).toString())
                 .pattern("i  ")
                 .pattern(" i ")
                 .pattern(" pb")
@@ -176,6 +196,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .save(recipeOutput, getItemId(ModItems.GUN));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.COMBAT, ModItems.BAYONETED_GUN)
+                .group(getItemId(ModItems.BAYONETED_GUN).toString())
                 .requires(ModItems.UPGRADE_GUN_BAYONET)
                 .requires(ModItems.GUN)
                 .unlockedBy("has_gun", has(ModItems.GUN))
