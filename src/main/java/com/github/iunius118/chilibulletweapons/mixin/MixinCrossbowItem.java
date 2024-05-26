a01e37a47ddc842c8fb05d843835a1fceca24114
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-05-27 07:12:51 +0900

    Fix reload duration of chili bullet guns when fired by non-players

--- src/main/java/com/github/iunius118/chilibulletweapons/mixin/MixinCrossbowItem.java
+++ src/main/java/com/github/iunius118/chilibulletweapons/mixin/MixinCrossbowItem.java
@@ -10,9 +10,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CrossbowItem.class)
 public class MixinCrossbowItem {
+    @Inject(method = "getChargeDuration", at = @At("HEAD"), cancellable = true)
+    private static void onGetChargeDuration(ItemStack itemStack, CallbackInfoReturnable<Integer> cir) {
+        if (itemStack.getItem() instanceof ChiliBulletGun chiliBulletGun) {
+            // When the entity has a chili bullet gun
+            int reloadDuration = chiliBulletGun.getReloadDuration(itemStack);
+            cir.setReturnValue(reloadDuration);
+        }
+    }
+
     @Inject(method = "performShooting", at = @At("HEAD"), cancellable = true)
     private static void onPerformShooting(Level level, LivingEntity livingEntity, InteractionHand hand, ItemStack itemStack, float velocity, float inaccuracy, CallbackInfo ci) {
         if (itemStack.getItem() instanceof ChiliBulletGun chiliBulletGun) {
