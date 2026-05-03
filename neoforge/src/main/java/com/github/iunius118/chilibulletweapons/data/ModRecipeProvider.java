7fd6342f7f9a9caeb7abe7ad45b9fb14c795f5c3
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-05-03 19:59:46 +0900

    Add sample recipe for customizing gun

--- neoforge/src/main/java/com/github/iunius118/chilibulletweapons/data/ModRecipeProvider.java
+++ neoforge/src/main/java/com/github/iunius118/chilibulletweapons/data/ModRecipeProvider.java
@@ -1,17 +1,33 @@
 package com.github.iunius118.chilibulletweapons.data;
 
+import com.github.iunius118.chilibulletweapons.Constants;
+import com.github.iunius118.chilibulletweapons.component.DyedGunColors;
+import com.github.iunius118.chilibulletweapons.component.GunContents;
+import com.github.iunius118.chilibulletweapons.component.ModDataComponents;
 import com.github.iunius118.chilibulletweapons.item.ModItems;
 import com.github.iunius118.chilibulletweapons.tags.ModItemTags;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.data.recipes.packs.VanillaRecipeProvider;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.Unit;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.component.Weapon;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.neoforged.neoforge.common.Tags;
 
 import java.util.concurrent.CompletableFuture;
@@ -125,6 +141,47 @@ public class ModRecipeProvider extends VanillaRecipeProvider {
                 .define('i', Tags.Items.INGOTS_IRON)
                 .unlockedBy("has_gun", has(ModItems.GUN))
                 .save(this.output);
+
+        // Sample of customized gun
+        var iconItem = getItem(Constants.Items.ICON_MAIN);
+        ShapedRecipeBuilder.shaped(items, RecipeCategory.COMBAT, getSampleGunTemplate())
+                .pattern("i  ")
+                .pattern(" # ")
+                .pattern(" pb")
+                .define('i', Tags.Items.INGOTS_IRON)
+                .define('#', iconItem)
+                .define('p', ItemTags.PLANKS)
+                .define('b', ModItems.CHILI_BULLET)
+                .unlockedBy("has_cbw_icon", has(iconItem))
+                .save(this.output, Constants.MOD_ID + ":sample/" + getItemId(ModItems.GUN).getPath() + "_customized");
+    }
+
+    private ItemStackTemplate getSampleGunTemplate() {
+        var itemAttributeModifiers = ItemAttributeModifiers.builder()
+                .add(Attributes.ATTACK_DAMAGE,
+                        new AttributeModifier(Item.BASE_ATTACK_DAMAGE_ID, 5, AttributeModifier.Operation.ADD_VALUE),
+                        EquipmentSlotGroup.MAINHAND)
+                .add(Attributes.ATTACK_SPEED,
+                        new AttributeModifier(Item.BASE_ATTACK_SPEED_ID, -2.8, AttributeModifier.Operation.ADD_VALUE),
+                        EquipmentSlotGroup.MAINHAND)
+                .build();
+        var enchantments = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        var itemEnchantments = new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
+        itemEnchantments.set(enchantments.getOrThrow(Enchantments.VANISHING_CURSE), 1);
+        var dataComponentPatch = DataComponentPatch.builder()
+                .set(ModDataComponents.GUN_CONTENTS, new GunContents(5, 1, 2, true))
+                .set(ModDataComponents.FIXED, Unit.INSTANCE)
+                .set(ModDataComponents.DYED_GUN_COLORS, new DyedGunColors(3355443, 10027008, 16764108, true))
+                .set(DataComponents.WEAPON, new Weapon(1))
+                .set(DataComponents.ATTRIBUTE_MODIFIERS, itemAttributeModifiers)
+                .set(DataComponents.CUSTOM_NAME, Component.literal("Unguis Accipitris"))
+                .set(DataComponents.ENCHANTMENTS, itemEnchantments.toImmutable())
+                .build();
+        return new ItemStackTemplate(ModItems.GUN, dataComponentPatch);
+    }
+
+    private Item getItem(Identifier id) {
+        return BuiltInRegistries.ITEM.getValue(id);
     }
 
     private Identifier getItemId(Item item) {
